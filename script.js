5a5961829968f218bb067a61de5b81bc88a0600e
Richard Hong <rhong04@tufts.edu>
2024-10-10 15:50:04 -0500

    adding js for rotating visual arrow to show which current attempt is undergoing

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-window.onload = () => {
-  console.log("testing");
+let currentAttempt = 1;
+
+const rotateArrowVisibility = () => {
+  if (currentAttempt > 6) return;
+  const currentArrow = document.querySelector("div.arrow[data-active]");
+  const nextArrow = document.getElementById(`row-${currentAttempt}`)
+    .children[0];
+  nextArrow.dataset.active = true;
+  if (currentArrow !== null) {
+    delete currentArrow.dataset.active;
+  }
 };
+
+window.onload = () => {
+  rotateArrowVisibility();
+};
+
+const btn = document.getElementById("btn");
+btn.addEventListener("click", () => {
+  currentAttempt++;
+  rotateArrowVisibility();
+});
