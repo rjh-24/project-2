0f92d46f6e28145e0c3457d277a782b4ce08389d
Richard Hong <rhong04@tufts.edu>
2024-10-29 12:09:40 -0500

    adding game logic for restarting game, adding api call to fetch new word and wordbank to pull static words from, adding html, js logic, and css for a restart game button

--- script.js
+++ script.js
@@ -1,12 +1,90 @@
+const responseContainer = document.getElementById("response-container");
+const restartGameBtn = document.getElementById("restart-game-btn");
+const guessCheckBtn = document.getElementById("guess-check-btn");
+
+const wordBank = [
+  "Apple",
+  "Bread",
+  "Crane",
+  "Dream",
+  "Eagle",
+  "Frame",
+  "Grace",
+  "Heart",
+  "Ideal",
+  "Joker",
+  "Knife",
+  "Lemon",
+  "Magic",
+  "Night",
+  "Ocean",
+  "Party",
+  "Queen",
+  "River",
+  "Stone",
+  "Trust",
+  "Urban",
+  "Voice",
+  "Water",
+  "Youth",
+  "Zebra",
+  "Angel",
+  "Bloom",
+  "Climb",
+  "Dance",
+  "Earth",
+  "Flame",
+  "Ghost",
+  "Happy",
+  "Honey",
+  "Light",
+];
+
 let currentAttempt = 0;
-let correctAnswer = "gulag";
+let correctAnswer;
 const correctAnswerLetterFrequency = {};
 
-correctAnswer.split("").forEach((letter) => {
-  correctAnswerLetterFrequency[letter]
-    ? correctAnswerLetterFrequency[letter]++
-    : (correctAnswerLetterFrequency[letter] = 1);
-});
+const getFrequency = (word) => {
+  word.split("").forEach((letter) => {
+    correctAnswerLetterFrequency[letter.toLowerCase()]
+      ? correctAnswerLetterFrequency[letter.toLowerCase()]++
+      : (correctAnswerLetterFrequency[letter.toLowerCase()] = 1);
+  });
+};
+
+const useWordBank = () => {
+  const randomIdx = Math.floor(Math.random() * 35);
+  correctAnswer = wordBank[randomIdx];
+  getFrequency(correctAnswer);
+};
+
+const getCurrentWord = (fetchWord = false) => {
+  if (!fetchWord) {
+    useWordBank();
+    return;
+  }
+
+  url = `https://wordle-api3.p.rapidapi.com/getcurrentword`;
+
+  const options = {
+    method: "GET",
+    headers: {
+      "x-rapidapi-key": "5c547f3788msh007f4139bb62e23p1dce91jsnd655fb0d4e13",
+      "x-rapidapi-host": "wordle-api3.p.rapidapi.com",
+    },
+  };
+
+  fetch(url, options)
+    .then((res) => res.json())
+    .then((data) => {
+      correctAnswer = data.word;
+      getFrequency(correctAnswer);
+    })
+    .catch((error) => {
+      console.log("error: ", error);
+      useWordBank();
+    });
+};
 
 const rotateArrowVisibility = () => {
   if (currentAttempt > 6) return;
@@ -19,23 +97,30 @@ const rotateArrowVisibility = () => {
   }
 };
 
-window.onload = () => {
-  // rotateArrowVisibility();
+const finishGame = (correctGuess = false) => {
+  let displayMessage;
+  displayMessage = correctGuess
+    ? `Congratulations! You took a total of ${currentAttempt} ${
+        currentAttempt === 1 ? `guess` : `guesses`
+      } to get the correct answer!`
+    : `Nice try! The answer was ${correctAnswer}.`;
+
+  window.alert(displayMessage);
 };
 
-const btn = document.getElementById("guess-check-btn");
-
-btn.addEventListener("click", () => {
+guessCheckBtn.addEventListener("click", () => {
   const userGuessInput = document.getElementById("user-guess");
   userGuessText = userGuessInput.value;
+
   currentAttempt++;
+
   rotateArrowVisibility();
 
   let frequencyCopy = { ...correctAnswerLetterFrequency };
 
   const boxes = document.getElementById(`row-${currentAttempt}`).children;
 
-  userGuessText.split("").map((letter, index) => {
+  userGuessText.split("").forEach((letter, index) => {
     boxes[index + 1].innerHTML = letter.toUpperCase();
 
     mappedAnswerLetter = correctAnswer[index].toLowerCase();
@@ -62,4 +147,38 @@ btn.addEventListener("click", () => {
     boxes[index + 1].classList.add("absent-letter");
     return;
   });
+
+  setTimeout(() => {
+    if (
+      userGuessText.toLowerCase() === correctAnswer.toLowerCase() ||
+      currentAttempt > 5
+    ) {
+      if (userGuessText.toLowerCase() === correctAnswer.toLowerCase())
+        finishGame(true);
+      if (currentAttempt > 5) finishGame();
+
+      responseContainer.style.display = "none";
+      restartGameBtn.style.display = "block";
+    }
+  }, 0);
 });
+
+restartGameBtn.addEventListener("click", () => {
+  responseContainer.style.display = "block";
+  restartGameBtn.style.display = "none";
+  document.getElementById("user-guess").value = "";
+  currentAttempt = 0;
+
+  const letterBoxes = document.querySelectorAll(".grid .letter-box");
+  letterBoxes.forEach((el) => {
+    el.classList.remove("correct-letter", "misplaced-letter", "absent-letter");
+    el.innerHTML = "";
+  });
+
+  const currentArrow = document.querySelector("div.arrow[data-active]");
+  delete currentArrow.dataset.active;
+});
+
+window.onload = () => {
+  getCurrentWord(true);
+};
